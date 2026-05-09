79ae0a1c0d20089ee00b31ea9766992b54f5f220
AnastasiyaBobko <a.s.bobko@gmail.com>
2026-05-09 19:20:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const brackets = [];
+  const pairs = new Map(bracketsConfig);
+
+  for (const char of str) {
+    const last = brackets[brackets.length - 1];
+
+    if (brackets.length && pairs.get(last) === char) {
+      brackets.pop();
+    } else {
+      brackets.push(char);
+    }
+  }
+
+  return brackets.length === 0;
 };
